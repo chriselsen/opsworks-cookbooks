0a61a0d1f3a7f5e7036e21eeb92c30380ddfcc99
Christian Elsen <chriselsen@gmx.net>
2013-12-31 15:14:02 -0800

    Mount gluster exports

--- gluster/recipes/configure.rb
+++ gluster/recipes/configure.rb
@@ -45,3 +45,25 @@ if is_first_node && instances.count > 1 then
         end
     #end
 end
+
+gluster_instances = node[:opsworks][:layers].fetch("gluster")[:instances]
+puts gluster_instances.inspect
+if gluster_instances.count > 0 then
+    server = gluster_instances.sort_by{|k,v| v[:booted_at] }[0][1][:private_ip]
+
+    node[:wordpress][:bind_mounts][:mounts].each do |dir, source|
+        directory source do
+            recursive true
+            action :create
+            mode "0755"
+        end
+
+        # mount -t glusterfs -o log-level=WARNING,log-file=/var/log/gluster.log 10.200.1.11:/test /mnt
+        mount "glusterfs mount point"  do
+            device "#{server}:/#{source}"
+            fstype "glusterfs"
+            options "log-level=WARNING,log-file=/var/log/gluster.log"
+            action :enable
+        end
+    end
+end
