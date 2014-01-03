ed63ec36719ebc784ca584ba507b906f6090c4e2
Christian Elsen <chriselsen@gmx.net>
2014-01-02 16:35:43 -0800

    Added support for external cron

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -33,3 +33,10 @@ node[:deploy].each do |app_name, deploy|
     end
 
 end
+
+cron "wordpress" do
+  hour "*"
+  minute "*/15"
+  weekday "*"
+  command "wget -q -O - http://localhost/wp-cron.php?doing_wp_cron >/dev/null 2>&1"
+end
