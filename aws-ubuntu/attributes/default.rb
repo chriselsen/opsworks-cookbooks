36129d9e8915ac95000a7d5bdb5ad4ff145c9e4b
Christian Elsen <chriselsen@gmx.net>
2014-01-03 16:31:38 -0800

    Added cookbook for creating swap on Linux for t1.micro instances

---
+++ aws-ubuntu/attributes/default.rb
@@ -0,0 +1,1 @@
+default[:aws-ubuntu][:swapsize] = "1024"
---
+++ aws-ubuntu/recipes/setup.rb
@@ -0,0 +1,17 @@
+#
+# Add memory swap: This should be used in instances with low RAM -
+# e.g. AWS t1.micro - to prevent "out of memory" issues.
+#
+swap_size = node[:aws-ubuntu][:swapsize]
+Chef::Log.debg("Creating SWAP with size #{swap_size} bytes")
+
+script "memory_swap" do
+  interpreter "bash"
+  user "root"
+  cwd "/"
+  code <<-EOH
+  /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=#{swap_size}
+  /sbin/mkswap /var/swap.1
+  /sbin/swapon /var/swap.1
+  EOH
+end
