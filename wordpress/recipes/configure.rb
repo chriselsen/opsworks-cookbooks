5f8de6355f2955991be6c6f9fea55fefbc34e108
Christian Elsen <chriselsen@gmx.net>
2014-01-09 22:00:23 -0800

    no message

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -32,10 +32,10 @@ node[:deploy].each do |app_name, deploy|
         )
     end
 
-	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
+	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
 
 	execute "Import Wordpress database backup" do
-		Chef::Log.debug("Command: if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi; exit 0")
+		Chef::Log.debug("Importing Wordpress database backup...")
 		command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi; exit 0"
 		action :run
 	end
