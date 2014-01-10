1b14ac01d473c3e8375c9a2f60b339b48fa8c94c
Christian Elsen <chriselsen@gmx.net>
2014-01-09 21:15:52 -0800

    no message

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,8 +1,8 @@
 node[:deploy].each do |application, deploy|
-  mysql_command = "/usr/bin/mysql -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
+  mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
 
   execute "Import Wordpress database backup" do
-    command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi"
+    command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi; exit 0"
     action :run
   end
 end
