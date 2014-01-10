4babbf92813bedb9d4d71a2d27fd32fd7e91db5e
Christian Elsen <chriselsen@gmx.net>
2014-01-09 20:16:32 -0800

    no message

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -5,3 +5,4 @@ node[:deploy].each do |application, deploy|
     command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi"
     action :run
   end
+end
