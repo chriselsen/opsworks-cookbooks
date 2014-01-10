543fa9afd8436ab0def154e64656cdc724d04c39
Christian Elsen <chriselsen@gmx.net>
2014-01-09 23:27:21 -0800

    no message

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -34,11 +34,17 @@ node[:deploy].each do |app_name, deploy|
 
 	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
 
-	execute "Import Wordpress database backup" do
-		Chef::Log.debug("Importing Wordpress database backup: if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi;")
-		command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi; exit 0"
-		action :run
+	script "memory_swap" do
+		interpreter "bash"
+		user "root"
+		cwd "#{deploy[:deploy_to]}/current/"
+		code <<-EOH
+			if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then 
+				#{mysql_command} < #{deploy[:deploy_to]}/current/*.sql;
+			fi;
+		EOH
 	end
+	
 end
 
 cron "wordpress" do
