30f25266d992aa3c507a8a5d59c2f26b6f3be553
Christian Elsen <chriselsen@gmx.net>
2014-01-09 20:58:51 -0800

    no message

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -32,6 +32,12 @@ node[:deploy].each do |app_name, deploy|
         )
     end
 
+	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
+
+	execute "Import Wordpress database backup" do
+		command "if ls #{deploy[:deploy_to]}/current/*.sql &> /dev/null; then #{mysql_command} < #{deploy[:deploy_to]}/current/*.sql; fi; exit 0"
+		action :run
+	end
 end
 
 cron "wordpress" do
