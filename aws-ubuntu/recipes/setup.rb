e430687e75be4ab7f376ac3c17efefc67dff5eb6
chriselsen <chriselsen@users.noreply.github.com>
2014-02-09 14:03:26 +0100

    Update setup.rb

--- aws-ubuntu/recipes/setup.rb
+++ aws-ubuntu/recipes/setup.rb
@@ -1,7 +1,7 @@
-#
-# Add memory swap: This should be used in instances with low RAM -
-# e.g. AWS t1.micro - to prevent "out of memory" issues.
-#
+# AWS OpsWorks Recipe for Ubuntu VMs to be executed during the Setup lifecycle phase
+# - Adds memory swap: This should be used in instances with low RAM -
+#   e.g. AWS t1.micro - to prevent "out of memory" issues.
+
 swap_size = node[:awsubuntu][:swapsize]
 Chef::Log.debug("Creating SWAP with size #{swap_size} bytes")
 
